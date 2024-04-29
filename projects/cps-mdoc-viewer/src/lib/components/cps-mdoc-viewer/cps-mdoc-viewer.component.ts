2b7506bf0379b7a22e9edf6243b8c04f3a35e1bd
Lukas Matta <lukasmatta@users.noreply.github.com>
2024-04-29 18:00:55 +0200

    Add support for custom color

--- projects/cps-mdoc-viewer/src/lib/components/cps-mdoc-viewer/cps-mdoc-viewer.component.ts
+++ projects/cps-mdoc-viewer/src/lib/components/cps-mdoc-viewer/cps-mdoc-viewer.component.ts
@@ -1,40 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from '../footer/footer.component';
 import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'cps-mdoc-viewer',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
-  templateUrl: 'cps-mdoc-viewer.component.html',
-  styles: `
-    :host {
-      .site-footer {
-        background-color: var(--cps-color-calm);
-
-        a {
-          display: block;
-          color: white;
-
-          &:hover {
-            text-decoration: none;
-          }
-
-          &:first-child {
-            margin-bottom: 10px;
-          }
-        }
-
-        .storyset-label {
-          font-size: 12px;
-        }
-      }
-    }
-  `
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  templateUrl: 'cps-mdoc-viewer.component.html'
 })
-export class CpsMdocViewerComponent {
-  scrollTop(event: MouseEvent): void {
-    event.preventDefault();
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  }
-}
+export class CpsMdocViewerComponent {}
--- projects/cps-mdoc-viewer/src/lib/components/footer/footer.component.ts
+++ projects/cps-mdoc-viewer/src/lib/components/footer/footer.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'lib-footer',
+  selector: 'footer-comp',
   standalone: true,
   imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrl: './footer.component.scss'
 })
-export class FooterComponent {}
+export class FooterComponent {
+  scrollTop(event: MouseEvent): void {
+    event.preventDefault();
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+}
