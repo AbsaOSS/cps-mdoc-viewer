908ed769833f0de4f882681f32282049becd5fc0
Lukas Matta <lukasmatta@users.noreply.github.com>
2024-04-29 15:07:21 +0200

    Add dependencies

--- projects/cps-mdoc-viewer/src/lib/cps-mdoc-viewer.component.ts
+++ projects/cps-mdoc-viewer/src/lib/cps-mdoc-viewer.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { CpsDividerComponent } from 'cps-ui-kit';
 
 @Component({
   selector: 'lib-cps-mdoc-viewer',
   standalone: true,
-  imports: [],
-  template: ` <p>cps-mdoc-viewer works!</p> `,
+  imports: [CpsDividerComponent],
+  template: ` <p>cps-mdoc-viewer works!</p><cps-divider />`,
   styles: ``
 })
 export class CpsMdocViewerComponent {}
