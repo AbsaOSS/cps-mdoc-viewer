fcc104da3e80d66f28ad36c57e408af180a779ef
Lukas Matta <lukasmatta@users.noreply.github.com>
2024-04-30 17:16:15 +0200

    Remove dummy service

--- projects/cps-mdoc-viewer/src/public-api.ts
+++ projects/cps-mdoc-viewer/src/public-api.ts
@@ -18,6 +18,5 @@
  * Public API Surface of cps-mdoc-viewer
  */
 
-export * from './lib/services/cps-mdoc-viewer.service';
 export * from './lib/components/cps-mdoc-viewer/cps-mdoc-viewer.component';
 export { provideCPSMDocViewer, CPSMDocViewerConfig } from './lib/lib.provider';
