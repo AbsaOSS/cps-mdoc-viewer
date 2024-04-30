6ed12ffb43fa21f00a84c74ec9b8fc5b47c8ff3e
Lukas Matta <lukasmatta@users.noreply.github.com>
2024-04-30 17:12:29 +0200

    Add license header to ts files

--- license-header.js
+++ license-header.js
@@ -13,25 +13,3 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-export interface Category {
-  sortedOrder: (string | MarkdownFile)[];
-  // TODO: Improve types
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  [key: string]: any;
-}
-
-export interface MarkdownFile {
-  title: string;
-  id: string;
-  filePath: string;
-  file: string;
-  weight: number;
-  content: string;
-  level: number;
-  toolbar_title?: string;
-  date?: Date;
-  // TODO: Improve types
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  [key: string]: any;
-}
